39669d491980a2515450ce6cee7e53e702ddaf98
Pablo Vega <pablo.vega@tiendeo.com>
2020-02-18 09:06:59 +0100

    use level rules

--- constants/customRules.js
+++ constants/customRules.js
@@ -2,7 +2,7 @@ const prettierOptions = require('../.prettierrc.js');
 const LEVEL_RULES = require('./levelRules');
 
 module.exports = {
-  'react-hooks/rules-of-hooks': 'error',
-  'react-hooks/exhaustive-deps': 'warn',
+  'react-hooks/rules-of-hooks': LEVEL_RULES.ERROR,
+  'react-hooks/exhaustive-deps': LEVEL_RULES.WARNING,
   'prettier/prettier': [LEVEL_RULES.ERROR, prettierOptions],
 };
